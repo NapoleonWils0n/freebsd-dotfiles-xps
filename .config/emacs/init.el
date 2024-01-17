1a80a2b22cd2cc5b5f731e20e12af3bbd0232cba
Daniel J Wilcox <danieljwilcox@gmail.com>
2024-01-17 15:01:03 +0000

    org mode video link

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -698,7 +698,7 @@
 
 ;; create a video: link type that opens a url using mpv-play-url
 (org-link-set-parameters "video"
-                         :follow #'mpv-play-url
+                         :follow #'mpv-play-remote-video
                          :complete #'org-mpv-complete-url
                          :store #'org-video-store-link)
 
@@ -710,6 +710,19 @@
        :type "video"
        :link link
        :description description))
+
+
+;; mpv-play-remote-video
+(defun mpv-play-remote-video (url &rest args)
+  "Start an mpv process playing the video stream at URL."
+  (interactive)
+  (unless (mpv--url-p url)
+    (user-error "Invalid argument: `%s' (must be a valid URL)" url))
+  (if (not mpv--process)
+      ;; mpv isnt running play file
+      (mpv-start url)
+      ;; mpv running append file to playlist
+    (mpv--playlist-append url)))
 
 
 ;; create a mpv: link type that opens a file using mpv-play
